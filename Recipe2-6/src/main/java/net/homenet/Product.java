4beac4dd5c2e2f6bf302ae749b217bbfe2a0942f
Lee Sangdae <mousesd@gmail.com>
2018-10-07 21:02:58 +0900

    Recipe 2-6, #1)

---
+++ Recipe2-6/src/main/java/net/homenet/Product.java
@@ -0,0 +1,21 @@
+package net.homenet;
+
+import java.text.DecimalFormat;
+
+public abstract class Product {
+    private String name;
+    private double price;
+    private double discount;
+
+    Product(String name, double price, double discount) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat formatter = new DecimalFormat("#%");
+        return name + " " + price + " (" + formatter.format(discount) + " discount!)";
+    }
+}
---
+++ Recipe2-6/src/main/java/net/homenet/ShopConfiguration.java
@@ -0,0 +1,48 @@
+package net.homenet;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@SuppressWarnings("SpringFacetCodeInspection")
+@Configuration
+@ComponentScan("net.homenet")
+@PropertySource("classpath:discounts.properties")
+public class ShopConfiguration {
+
+    @Value("${specialcustomer.discount:0}")
+    private double specialCustomerDiscount;
+    @Value("${summer.discount:0}")
+    private double specialSummerDiscount;
+    @Value("${endofyear.discount:0}")
+    private double specialEndofyearDiscount;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public Product battery() {
+        Battery battery = new Battery("AAA", 2.5, specialCustomerDiscount);
+        battery.setRechargeable(true);
+        return battery;
+    }
+
+    @Bean
+    public Product cdrw() {
+        Disc cdrw = new Disc("CD-RW", 1.5, specialSummerDiscount);
+        cdrw.setCapacity(700);
+        return cdrw;
+    }
+
+    @Bean
+    public Product dvdrw() {
+        Disc dvdrw = new Disc("DVD-RW", 3.0, specialEndofyearDiscount);
+        dvdrw.setCapacity(700);
+        return dvdrw;
+    }
+}
