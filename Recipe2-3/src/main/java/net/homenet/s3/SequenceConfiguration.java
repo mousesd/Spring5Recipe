bf854873c21294f0f57a265a6d9067def41fa7ce
Lee Sangdae <mousesd@gmail.com>
2018-10-07 11:13:59 +0900

    Recipe 2-3, #3)

---
+++ Recipe2-3/src/main/java/net/homenet/s3/SequenceConfiguration.java
@@ -0,0 +1,27 @@
+package net.homenet.s3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SequenceConfiguration {
+    @Bean
+    public DatePrefixGenerator datePrefixGenerator() {
+        DatePrefixGenerator generator = new DatePrefixGenerator();
+        generator.setPattern("yyyyMMdd");
+        return generator;
+    }
+
+    @Bean
+    public NumberPrefixGenerator numberPrefixGenerator() {
+        return new NumberPrefixGenerator();
+    }
+
+    @Bean
+    public SequenceGenerator sequenceGenerator() {
+        SequenceGenerator generator = new SequenceGenerator();
+        generator.setInitial(10000);
+        generator.setSuffix("A");
+        return generator;
+    }
+}
---
+++ Recipe2-3/src/main/java/net/homenet/s3/SequenceGenerator.java
@@ -0,0 +1,39 @@
+package net.homenet.s3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SuppressWarnings("SameParameterValue")
+class SequenceGenerator {
+    @Autowired
+    private PrefixGenerator[] prefixGenerators;
+    private String suffix;
+    private int initial;
+    private final AtomicInteger counter = new AtomicInteger();
+
+    void setPrefixGenerators(PrefixGenerator[] prefixGenerators) {
+        this.prefixGenerators = prefixGenerators;
+    }
+
+    void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    void setInitial(int initial) {
+        this.initial = initial;
+    }
+
+    String getSequence() {
+        StringBuilder builder = new StringBuilder();
+        for (PrefixGenerator generator : prefixGenerators) {
+            builder.append(generator.getPrefix());
+            builder.append("-");
+        }
+        builder.append(initial)
+            .append(counter.getAndIncrement())
+            .append(suffix);
+        return builder.toString();
+    }
+}
+
