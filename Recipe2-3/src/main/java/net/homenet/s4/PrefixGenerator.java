0252a582b9207c53479e8793677a12f0131167ce
Lee Sangdae <mousesd@gmail.com>
2018-10-07 11:23:52 +0900

    Recipe 2-3, #4)

---
+++ Recipe2-3/src/main/java/net/homenet/s4/PrefixGenerator.java
@@ -0,0 +1,5 @@
+package net.homenet.s4;
+
+public interface PrefixGenerator {
+    String getPrefix();
+}
---
+++ Recipe2-3/src/main/java/net/homenet/s4/SequenceGenerator.java
@@ -0,0 +1,33 @@
+package net.homenet.s4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SuppressWarnings("SameParameterValue")
+class SequenceGenerator {
+    private PrefixGenerator prefixGenerator;
+    private String suffix;
+    private int initial;
+    private final AtomicInteger counter = new AtomicInteger();
+
+    @Autowired
+    public void setPrefixGenerator(PrefixGenerator prefixGenerator) {
+        this.prefixGenerator = prefixGenerator;
+    }
+
+    void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    void setInitial(int initial) {
+        this.initial = initial;
+    }
+
+    String getSequence() {
+        return prefixGenerator.getPrefix()
+            + (initial + counter.getAndIncrement())
+            + suffix;
+    }
+}
+
