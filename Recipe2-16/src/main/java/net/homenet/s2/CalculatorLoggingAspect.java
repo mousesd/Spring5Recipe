dc58f762226a096337d911806a749f5e86234983
Lee Sangdae <mousesd@gmail.com>
2018-10-14 13:30:21 +0900

    Recipe 2-16, #2)

--- Recipe2-16/src/main/java/net/homenet/s2/CalculatorLoggingAspect.java
+++ Recipe2-16/src/main/java/net/homenet/s2/CalculatorLoggingAspect.java
@@ -3,7 +3,6 @@ package net.homenet.s2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
