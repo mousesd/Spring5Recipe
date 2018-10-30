c4d7dede456e8b9a41a041cb5b0060613878c4ad
Lee Sangdae <mousesd@gmail.com>
2018-10-30 11:50:59 +0900

    Recipe 3-11

---
+++ Chapter03/Recipe3-11/src/main/java/net/homenet/configuration/ExceptionHandlingAdvice.java
@@ -0,0 +1,18 @@
+package net.homenet.configuration;
+
+import net.homenet.service.ReservationNotAvailableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlingAdvice {
+    @ExceptionHandler(ReservationNotAvailableException.class)
+    public String handle(ReservationNotAvailableException ex) {
+        return "reservationNotAvailable";
+    }
+
+    @ExceptionHandler
+    public String handleDefault(Exception ex) {
+        return "error";
+    }
+}
