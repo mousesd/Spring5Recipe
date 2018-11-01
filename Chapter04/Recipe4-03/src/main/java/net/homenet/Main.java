4a982f67f4e7f6208cca48eec852ff8fc12684ae
Lee Sangdae <mousesd@gmail.com>
2018-11-02 00:05:04 +0900

    Recipe 4-3, #3)

--- Chapter04/Recipe4-03/src/main/java/net/homenet/Main.java
+++ Chapter04/Recipe4-03/src/main/java/net/homenet/Main.java
@@ -14,12 +14,18 @@ public class Main {
         //System.out.println(result);
 
         //# 2.
-        final String uri = "http://localhost:8080/member/{memberId}";
-        Map<String, String> params = new HashMap<>();
-        params.put("memberId", "1");
+        //final String uri = "http://localhost:8080/member/{memberId}";
+        //Map<String, String> params = new HashMap<>();
+        //params.put("memberId", "1");
+        //
+        //RestTemplate restTemplate = new RestTemplate();
+        //String result = restTemplate.getForObject(uri, String.class, params);
+        //System.out.println(result);
 
+        //# 3.
+        final String uri = "http://localhost:8080/members.xml";
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class, params);
-        System.out.println(result);
+        Members members = restTemplate.getForObject(uri, Members.class);
+        System.out.println(members);
     }
 }
