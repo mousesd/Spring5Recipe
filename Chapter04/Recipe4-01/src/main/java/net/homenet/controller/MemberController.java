ebd19dc343564f874ff8e816350035b6b5ddf618
Lee Sangdae <mousesd@gmail.com>
2018-11-01 16:41:44 +0900

    Recipe 4-1

--- Chapter04/Recipe4-01/src/main/java/net/homenet/controller/MemberController.java
+++ Chapter04/Recipe4-01/src/main/java/net/homenet/controller/MemberController.java
@@ -4,6 +4,8 @@ import net.homenet.domain.Member;
 import net.homenet.domain.Members;
 import net.homenet.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +30,10 @@ public class MemberController {
 
     @RequestMapping("/member/{memberId}")
     @ResponseBody
-    public Member getMember(@PathVariable("memberId") long memberId) {
-        return memberService.find(memberId);
+    public ResponseEntity<Member> getMember(@PathVariable("memberId") long memberId) {
+        Member member = memberService.find(memberId);
+        if (member != null)
+            return new ResponseEntity<>(member, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
