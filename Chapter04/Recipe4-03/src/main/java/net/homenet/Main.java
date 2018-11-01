ba66f3f45151e51952f0baa848aa989cd151ffd6
Lee Sangdae <mousesd@gmail.com>
2018-11-02 00:02:30 +0900

    Recipe 4-3, #2)

--- Chapter04/Recipe4-03/src/main/java/net/homenet/Main.java
+++ Chapter04/Recipe4-03/src/main/java/net/homenet/Main.java
@@ -2,11 +2,24 @@ package net.homenet;
 
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        final String uri = "http://localhost:8080/members.xml";
+        //# 1.
+        //final String uri = "http://localhost:8080/members.xml";
+        //RestTemplate restTemplate = new RestTemplate();
+        //String result = restTemplate.getForObject(uri, String.class);
+        //System.out.println(result);
+
+        //# 2.
+        final String uri = "http://localhost:8080/member/{memberId}";
+        Map<String, String> params = new HashMap<>();
+        params.put("memberId", "1");
+
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
+        String result = restTemplate.getForObject(uri, String.class, params);
         System.out.println(result);
     }
 }
