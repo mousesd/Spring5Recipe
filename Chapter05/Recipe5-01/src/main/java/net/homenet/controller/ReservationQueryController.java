f826c334281cb3f652e76a092505f7875869815f
Lee Sangdae <mousesd@gmail.com>
2018-11-03 10:27:03 +0900

    Recipe 5-1, #2)

--- Chapter05/Recipe5-01/src/main/java/net/homenet/controller/ReservationQueryController.java
+++ Chapter05/Recipe5-01/src/main/java/net/homenet/controller/ReservationQueryController.java
@@ -3,24 +3,27 @@ package net.homenet.controller;
 import net.homenet.domain.Reservation;
 import net.homenet.service.ReservationService;
 import net.homenet.util.Delayer;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
 
 @Controller
 @RequestMapping("/reservationQuery")
 public class ReservationQueryController {
     private final ReservationService reservationService;
+    private final TaskExecutor taskExecutor;
 
-    public ReservationQueryController(ReservationService reservationService) {
+    public ReservationQueryController(ReservationService reservationService, TaskExecutor taskExecutor) {
         this.reservationService = reservationService;
+        this.taskExecutor = taskExecutor;
     }
 
     //@GetMapping
@@ -33,6 +36,7 @@ public class ReservationQueryController {
     @GetMapping
     public void setupForm() { }
 
+    //# 1.Sync
     //@PostMapping
     //public String submitForm(@RequestParam("courtName") String courtName, Model model) {
     //    List<Reservation> reservations = Collections.emptyList();
@@ -43,16 +47,33 @@ public class ReservationQueryController {
     //    return "reservationQuery";
     //}
 
+    //# 2.Async #1)
+    //@PostMapping
+    //public Callable<String> submitForm(@RequestParam("courtName") String courtName, Model model) {
+    //    return () -> {
+    //        List<Reservation> reservations = Collections.emptyList();
+    //        if (courtName != null) {
+    //            Delayer.randomDelay();
+    //            reservations = reservationService.query(courtName);
+    //        }
+    //        model.addAttribute("reservations", reservations);
+    //        return "reservationQuery";
+    //    };
+    //}
+
+    //# 3.Async #2)
     @PostMapping
-    public Callable<String> submitForm(@RequestParam("courtName") String courtName, Model model) {
-        return () -> {
+    public DeferredResult<String> submitForm(@RequestParam("courtName") String courtName, Model model) {
+        DeferredResult<String> result = new DeferredResult<>();
+        taskExecutor.execute(() -> {
             List<Reservation> reservations = Collections.emptyList();
             if (courtName != null) {
                 Delayer.randomDelay();
                 reservations = reservationService.query(courtName);
             }
             model.addAttribute("reservations", reservations);
-            return "reservationQuery";
-        };
+            result.setResult("reservationQuery");
+        });
+        return result;
     }
 }
