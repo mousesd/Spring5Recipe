bd3f3026855b4ba71bf173e2be777bb7c0a0f457
Lee Sangdae <mousesd@gmail.com>
2018-11-05 11:36:13 +0900

    Recipe 5-2, #2)

--- Chapter05/Recipe5-02/src/main/java/net/homenet/controller/ReservationQueryController.java
+++ Chapter05/Recipe5-02/src/main/java/net/homenet/controller/ReservationQueryController.java
@@ -3,18 +3,20 @@ package net.homenet.controller;
 import net.homenet.domain.Reservation;
 import net.homenet.service.ReservationService;
 import net.homenet.util.Delayer;
-import org.springframework.core.task.AsyncListenableTaskExecutor;
 import org.springframework.core.task.TaskExecutor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -46,11 +48,43 @@ public class ReservationQueryController {
         };
     }
 
+    //# 1.GET http://localhost:8080/reservationQuery?courtName=Tennis #1
+    //# - 예외 발생!
+    //  org.apache.catalina.connector.CoyoteAdapter.checkRecycled Encountered a non-recycled request and recycled it forcedly.
+    //  org.apache.catalina.connector.CoyoteAdapter$RecycleRequiredException
+    //  at org.apache.catalina.connector.CoyoteAdapter.checkRecycled(CoyoteAdapter.java:501)
+    //  at org.apache.coyote.http11.Http11Processor.recycle(Http11Processor.java:1706)
+    //  at org.apache.coyote.AbstractProtocol$ConnectionHandler.release(AbstractProtocol.java:988)
+    //  at org.apache.coyote.AbstractProtocol$ConnectionHandler.process(AbstractProtocol.java:922)
+    //  at org.apache.tomcat.util.net.NioEndpoint$SocketProcessor.doRun(NioEndpoint.java:1498)
+    //  at org.apache.tomcat.util.net.SocketProcessorBase.run(SocketProcessorBase.java:49)
+    //  at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
+    //  at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
+    //  at org.apache.tomcat.util.threads.TaskThread$WrappingRunnable.run(TaskThread.java:61)
+    //  at java.lang.Thread.run(Thread.java:748)
+    //@GetMapping(params = "courtName")
+    //public ResponseBodyEmitter find(@RequestParam("courtName") String courtName) {
+    //    final ResponseBodyEmitter emitter = new ResponseBodyEmitter();
+    //    taskExecutor.execute(() -> {
+    //        Collection<Reservation> reservations = reservationService.query(courtName);
+    //        try {
+    //            for (Reservation reservation : reservations) {
+    //                emitter.send(reservation);
+    //            }
+    //            emitter.complete();
+    //        } catch (IOException e) {
+    //            emitter.completeWithError(e);
+    //        }
+    //    });
+    //    return emitter;
+    //}
+
+    //# 2.
     @GetMapping(params = "courtName")
-    public ResponseBodyEmitter find(@RequestParam("courtName") String courtName) {
+    public ResponseEntity<ResponseBodyEmitter> find(@RequestParam("courtName") String courtName) {
         ResponseBodyEmitter emitter = new ResponseBodyEmitter();
         taskExecutor.execute(() -> {
-            List<Reservation> reservations = reservationService.query(courtName);
+            Collection<Reservation> reservations = reservationService.query(courtName);
             try {
                 for (Reservation reservation : reservations) {
                     emitter.send(reservation);
@@ -60,6 +94,9 @@ public class ReservationQueryController {
                 emitter.completeWithError(e);
             }
         });
-        return emitter;
+
+        return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT)
+            .header("Custom-Header", "Custom-Value")
+            .body(emitter);
     }
 }
