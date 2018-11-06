1ed80f34d9d115800bf860a3966c80586eac16cb
Lee Sangdae <mousesd@gmail.com>
2018-11-06 19:15:33 +0900

    Recipe 5-5

--- Chapter05/Recipe5-05/src/main/java/net/homenet/controller/WelcomeController.java
+++ Chapter05/Recipe5-05/src/main/java/net/homenet/controller/WelcomeController.java
@@ -11,7 +11,9 @@ import java.time.LocalDate;
 @Controller
 @RequestMapping("/welcome")
 public class WelcomeController {
-    //# TODO: @GetMapping 를 주석처리하면 ServletException 이 발생하는지 확인
+    //# @GetMapping 를 주석처리하면 ServletException 이 발생하는지 확인
+    //# - ServletException 발생하지 않음
+    //# - 브라우저에서 확인시 404 NOT FOUND 만 표시됨
     @GetMapping
     public String welcome(Model model) {
         model.addAttribute("today", Mono.just(LocalDate.now()));
