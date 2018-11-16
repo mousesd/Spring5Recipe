16448a5b12eb491f604af7d0cfcd478710fb2659
Lee Sangdae <mousesd@gmail.com>
2018-11-16 16:41:14 +0900

    Recipe 7-3, #2)

--- Chapter07/Recipe7-03/src/main/java/net/homenet/configuration/TodoSecurityConfiguration.java
+++ Chapter07/Recipe7-03/src/main/java/net/homenet/configuration/TodoSecurityConfiguration.java
@@ -1,11 +1,17 @@
 package net.homenet.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +19,16 @@ public class TodoSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //# 1.Authenticate user with In-Memory definitions
-        auth.inMemoryAuthentication()
-            .withUser("mousesd@gmail.com").password("{noop}gmail").authorities("USER", "ADMIN")
-            .and()
-            .withUser("mousesd@outlook.com").password("{noop}outlook").authorities("USER")
-            .and()
-            .withUser("mousesd@icloud.com").password("{noop}icloud").disabled(true).authorities("USER", "ADMIN");
+        //auth.inMemoryAuthentication()
+        //    .withUser("mousesd@gmail.com").password("{noop}gmail").authorities("USER", "ADMIN")
+        //    .and()
+        //    .withUser("mousesd@outlook.com").password("{noop}outlook").authorities("USER")
+        //    .and()
+        //    .withUser("mousesd@icloud.com").password("{noop}icloud").disabled(true).authorities("USER", "ADMIN");
+
+        //# 2.Authenticate users against a Database
+        auth.jdbcAuthentication()
+            .dataSource(dataSource());
     }
 
     @Override
@@ -39,5 +49,15 @@ public class TodoSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/logout-success.jsp")
             .and().headers()
             .and().httpBasic().disable();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+            .setType(EmbeddedDatabaseType.H2)
+            .setName("board")
+            .addScript("classpath:/schema.sql")
+            .addScript("classpath:/data.sql")
+            .build();
     }
 }
--- Chapter07/Recipe7-03/src/main/java/net/homenet/configuration/TodoWebConfiguration.java
+++ Chapter07/Recipe7-03/src/main/java/net/homenet/configuration/TodoWebConfiguration.java
@@ -3,14 +3,10 @@ package net.homenet.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebMvc
@@ -29,13 +25,13 @@ public class TodoWebConfiguration implements WebMvcConfigurer {
         return viewResolver;
     }
 
-    @Bean
-    public DataSource dataSource() {
-        // url='jdbc:h2:mem:board;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false', username='sa'
-        return new EmbeddedDatabaseBuilder()
-            .setType(EmbeddedDatabaseType.H2)
-            .setName("board")
-            .addScript("classpath:/schema.sql")
-            .build();
-    }
+    //@Bean
+    //public DataSource dataSource() {
+    //    // url='jdbc:h2:mem:board;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=false', username='sa'
+    //    return new EmbeddedDatabaseBuilder()
+    //        .setType(EmbeddedDatabaseType.H2)
+    //        .setName("board")
+    //        .addScript("classpath:/schema.sql")
+    //        .build();
+    //}
 }
