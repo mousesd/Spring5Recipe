e14973daf540d1314064db6e5a6a2776288aee4c
Lee Sangdae <mousesd@gmail.com>
2018-11-27 16:51:55 +0900

    Recipe 8-2, Initialize

---
+++ Chapter08/Recipe8-02/src/main/java/net/homenet/configuration/MobileAppConfiguration.java
@@ -0,0 +1,20 @@
+package net.homenet.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("net.homenet")
+public class MobileAppConfiguration {
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
---
+++ Chapter08/Recipe8-02/src/main/java/net/homenet/configuration/MobileAppInitializer.java
@@ -0,0 +1,28 @@
+package net.homenet.configuration;
+
+import net.homenet.filter.DeviceResolverRequestFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class MobileAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{ MobileAppConfiguration.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{ "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{ new DeviceResolverRequestFilter() };
+    }
+}
