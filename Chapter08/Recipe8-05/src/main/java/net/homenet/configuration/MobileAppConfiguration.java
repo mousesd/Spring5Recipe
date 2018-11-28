ddf39164e6e84289b0eca20b0d4b390a3fb6f417
Lee Sangdae <mousesd@gmail.com>
2018-11-28 10:42:29 +0900

    Recipe 8-5, Implement site switching

--- Chapter08/Recipe8-05/src/main/java/net/homenet/configuration/MobileAppConfiguration.java
+++ Chapter08/Recipe8-05/src/main/java/net/homenet/configuration/MobileAppConfiguration.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mobile.device.DeviceResolverHandlerInterceptor;
 import org.springframework.mobile.device.site.SitePreferenceHandlerInterceptor;
+import org.springframework.mobile.device.switcher.SiteSwitcherHandlerInterceptor;
 import org.springframework.mobile.device.view.LiteDeviceDelegatingViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+@SuppressWarnings("Duplicates")
 @Configuration
 @EnableWebMvc
 @ComponentScan("net.homenet")
@@ -18,7 +20,8 @@ public class MobileAppConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new DeviceResolverHandlerInterceptor());
-        registry.addInterceptor(new SitePreferenceHandlerInterceptor());
+        //registry.addInterceptor(new SitePreferenceHandlerInterceptor());
+        registry.addInterceptor(siteSwitcherHandlerInterceptor());
     }
 
     //@Override
@@ -43,5 +46,21 @@ public class MobileAppConfiguration implements WebMvcConfigurer {
         viewResolver.setTabletPrefix("tablet/");
         viewResolver.setOrder(1);
         return viewResolver;
+    }
+
+    @Bean
+    public SiteSwitcherHandlerInterceptor siteSwitcherHandlerInterceptor() {
+        //# 1.http://m.yourdomain.mobi:8080/home
+        //return SiteSwitcherHandlerInterceptor.mDot("yourdomain.com", true);
+
+        //# 2.http://yourdomain.mobi:8080/home
+        //return SiteSwitcherHandlerInterceptor.dotMobi("yourdomain.com", true);
+
+        //# 3.http://localhost:8080/home/mobile
+        //return SiteSwitcherHandlerInterceptor.urlPath("/mobile", "/tablet", "home");
+
+        //# 4.http://mobile.yourdomain.com:8080/home
+        return SiteSwitcherHandlerInterceptor.standard("yourdomain.com"
+            , "mobile.yourdomain.com", "tablet.yourdomain.com", "*.yourdomain.com");
     }
 }
