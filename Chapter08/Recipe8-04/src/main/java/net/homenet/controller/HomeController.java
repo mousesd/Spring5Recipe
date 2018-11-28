8c5a89964d82130fdedaea2b0412f421d3f23252
Lee Sangdae <mousesd@gmail.com>
2018-11-28 09:18:23 +0900

    Recipe 8-4, #1) Use Device, DeviceUtils class in controller

--- Chapter08/Recipe8-04/src/main/java/net/homenet/controller/HomeController.java
+++ Chapter08/Recipe8-04/src/main/java/net/homenet/controller/HomeController.java
@@ -1,5 +1,7 @@
 package net.homenet.controller;
 
+import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.DeviceUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 public class HomeController {
     @RequestMapping("/home")
     public String index(HttpServletRequest request) {
-        return "home";
+        Device device = DeviceUtils.getCurrentDevice(request);
+        if (device.isMobile()) {
+            return "mobile/home";
+        } else if (device.isTablet()) {
+            return "tablet/home";
+        } else {
+            return "home";
+        }
     }
 }
