79e9c63d70d73d8c3ac193ca5efcbfa9434e6c6e
Lee Sangdae <mousesd@gmail.com>
2018-12-03 16:54:42 +0900

    Recipe 9-8, Initialize

---
+++ Chapter09/Recipe9-08/src/main/java/net/homenet/configuration/CourseConfiguration.java
@@ -0,0 +1,79 @@
+package net.homenet.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import net.homenet.CourseDao;
+import net.homenet.JpaCourseDaoImpl;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.dialect.PostgreSQL95Dialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@SuppressWarnings("Duplicates")
+@Configuration
+public class CourseConfiguration {
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(AvailableSettings.DIALECT, PostgreSQL95Dialect.class.getName());
+        properties.setProperty(AvailableSettings.SHOW_SQL, String.valueOf(true));
+        properties.setProperty(AvailableSettings.HBM2DDL_AUTO, "update");
+        return properties;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/course");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("sqladmin");
+        dataSource.setMinimumIdle(2);
+        dataSource.setMaximumPoolSize(5);
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean(DataSource dataSource) {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setHibernateProperties(hibernateProperties());
+        sessionFactoryBean.setPackagesToScan("net.homenet");
+        return sessionFactoryBean;
+    }
+
+    //# Use a LocalEntityManagerFactoryBean
+    //@Bean
+    //public LocalEntityManagerFactoryBean entityManagerFactory() {
+    //    LocalEntityManagerFactoryBean lemf = new LocalEntityManagerFactoryBean();
+    //    lemf.setPersistenceUnitName("course");
+    //    return lemf;
+    //}
+
+    private JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(true);
+        adapter.setDatabasePlatform(PostgreSQL95Dialect.class.getName());
+        return adapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource);
+        emf.setPackagesToScan("net.homenet");
+        emf.setJpaVendorAdapter(jpaVendorAdapter());
+        return emf;
+    }
+
+    @Bean
+    public CourseDao courseDao(EntityManagerFactory entityManagerFactory) {
+        return new JpaCourseDaoImpl(entityManagerFactory);
+    }
+}
