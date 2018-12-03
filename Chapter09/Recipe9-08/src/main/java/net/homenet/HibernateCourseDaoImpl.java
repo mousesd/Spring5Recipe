005236438abf59e49b4ad7f128fce232bd9bfe9f
Lee Sangdae <mousesd@gmail.com>
2018-12-03 17:11:17 +0900

    Recipe 9-8, Persist objects with Hibernate's contextual sessions

--- Chapter09/Recipe9-08/src/main/java/net/homenet/HibernateCourseDaoImpl.java
+++ Chapter09/Recipe9-08/src/main/java/net/homenet/HibernateCourseDaoImpl.java
@@ -2,76 +2,48 @@ package net.homenet;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @SuppressWarnings({ "WeakerAccess", "JpaQlInspection", "Duplicates" })
+@Repository("courseDao")
 public class HibernateCourseDaoImpl implements CourseDao {
     private final SessionFactory sessionFactory;
 
     public HibernateCourseDaoImpl(SessionFactory sessionFactory) {
-        //# Native configuration
-        //Configuration configuration = new Configuration()
-        //    .setProperty(AvailableSettings.URL, "jdbc:postgresql://localhost:5432/course")
-        //    .setProperty(AvailableSettings.USER, "postgres")
-        //    .setProperty(AvailableSettings.PASS, "sqladmin")
-        //    .setProperty(AvailableSettings.DIALECT, PostgreSQL95Dialect.class.getName())
-        //    .setProperty(AvailableSettings.SHOW_SQL, String.valueOf(true))
-        //    .setProperty(AvailableSettings.HBM2DDL_AUTO, "update")
-        //    //.addClass(Course.class);  //# XML mapping
-        //    .addAnnotatedClass(Course.class);
-        //this.sessionFactory = configuration.buildSessionFactory();
-
         this.sessionFactory = sessionFactory;
     }
 
+    @Transactional
     @Override
     public Course store(Course course) {
-        Session session = sessionFactory.openSession();
-        Transaction tx = session.getTransaction();
-        try {
-            tx.begin();
-            session.saveOrUpdate(course);
-            tx.commit();
-            return course;
-        } catch (RuntimeException e) {
-            tx.rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(course);
+        return course;
     }
 
+    @Transactional
     @Override
     public void delete(Long courseId) {
-        Session session = sessionFactory.openSession();
-        Transaction tx = session.getTransaction();
-        try {
-            tx.begin();
-            Course course = session.get(Course.class, courseId);
-            session.delete(course);
-            tx.commit();
-        } catch (RuntimeException e) {
-            tx.rollback();
-            throw e;
-        } finally {
-            session.close();
-        }
+        Session session = sessionFactory.getCurrentSession();
+        Course course = session.get(Course.class, courseId);
+        session.delete(course);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Course findById(Long courseId) {
-        try (Session session = sessionFactory.openSession()) {
-            return session.get(Course.class, courseId);
-        }
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Course.class, courseId);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Course> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("SELECT ID, TITLE, BEGIN_DATE, END_DATE, FEE FROM COURSE", Course.class)
-                .list();
-        }
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery("SELECT ID, TITLE, BEGIN_DATE, END_DATE, FEE FROM COURSE", Course.class)
+            .list();
     }
 }
--- Chapter09/Recipe9-08/src/main/java/net/homenet/configuration/CourseConfiguration.java
+++ Chapter09/Recipe9-08/src/main/java/net/homenet/configuration/CourseConfiguration.java
@@ -2,22 +2,24 @@ package net.homenet.configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
 import net.homenet.CourseDao;
-import net.homenet.JpaCourseDaoImpl;
+import net.homenet.HibernateCourseDaoImpl;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.dialect.PostgreSQL95Dialect;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @SuppressWarnings("Duplicates")
 @Configuration
+@EnableTransactionManagement
+@ComponentScan("net.homenet")
 public class CourseConfiguration {
     private Properties hibernateProperties() {
         Properties properties = new Properties();
@@ -47,33 +49,13 @@ public class CourseConfiguration {
         return sessionFactoryBean;
     }
 
-    //# Use a LocalEntityManagerFactoryBean
+    @Bean
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+        return new HibernateTransactionManager(sessionFactory);
+    }
+
     //@Bean
-    //public LocalEntityManagerFactoryBean entityManagerFactory() {
-    //    LocalEntityManagerFactoryBean lemf = new LocalEntityManagerFactoryBean();
-    //    lemf.setPersistenceUnitName("course");
-    //    return lemf;
+    //public CourseDao courseDao(SessionFactory sessionFactory) {
+    //    return new HibernateCourseDaoImpl(sessionFactory);
     //}
-
-    private JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setShowSql(true);
-        adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform(PostgreSQL95Dialect.class.getName());
-        return adapter;
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(dataSource);
-        emf.setPackagesToScan("net.homenet");
-        emf.setJpaVendorAdapter(jpaVendorAdapter());
-        return emf;
-    }
-
-    @Bean
-    public CourseDao courseDao(EntityManagerFactory entityManagerFactory) {
-        return new JpaCourseDaoImpl(entityManagerFactory);
-    }
 }
