1beafc5436839e6c15cefc36dc229751e8329fa6
Lee Sangdae <mousesd@gmail.com>
2018-12-04 16:06:55 +0900

    Recipe 10-4, #1) Use a TransactionTemplate

--- Chapter10/Recipe10-04/src/main/java/net/homenet/TransactionJdbcBookshop.java
+++ Chapter10/Recipe10-04/src/main/java/net/homenet/TransactionJdbcBookshop.java
@@ -6,10 +6,12 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 
-@SuppressWarnings({ "SqlDialectInspection", "WeakerAccess" })
+@SuppressWarnings({ "SqlDialectInspection", "WeakerAccess", "Duplicates" })
 public class TransactionJdbcBookshop extends JdbcDaoSupport implements Bookshop {
     private final PlatformTransactionManager transactionManager;
 
@@ -20,20 +22,17 @@ public class TransactionJdbcBookshop extends JdbcDaoSupport implements Bookshop 
 
     @Override
     public void purchase(String isbn, String username) {
-        TransactionDefinition definition = new DefaultTransactionDefinition();
-        TransactionStatus status = transactionManager.getTransaction(definition);
+        TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                int price = getJdbcTemplate()
+                    .queryForObject("SELECT price FROM book WHERE isbn = ?", Integer.class, isbn);
 
-        try {
-            int price = getJdbcTemplate()
-                .queryForObject("SELECT price FROM book WHERE isbn = ?", Integer.class, isbn);
+                getJdbcTemplate().update("UPDATE book_stock SET stock = stock - 1 WHERE isbn = ?", isbn);
 
-            getJdbcTemplate().update("UPDATE book_stock SET stock = stock - 1 WHERE isbn = ?", isbn);
-
-            getJdbcTemplate().update("UPDATE account SET balance = balance - ? WHERE username = ?", price, username);
-            transactionManager.commit(status);
-        } catch (DataAccessException e) {
-            transactionManager.rollback(status);
-            e.printStackTrace();
-        }
+                getJdbcTemplate().update("UPDATE account SET balance = balance - ? WHERE username = ?", price, username);
+            }
+        });
     }
 }
