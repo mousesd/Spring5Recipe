fc028488d382939b859e452db9679fa45a0f7666
Lee Sangdae <mousesd@gmail.com>
2019-01-02 09:58:22 +0900

    Chapter11:App02: #1) Set up Spring Batch's infrastructure(PostgreSQL)

---
+++ Chapter11/App02/src/main/java/net/homenet/configuration/BatchConfiguration.java
@@ -0,0 +1,54 @@
+package net.homenet.configuration;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableBatchProcessing
+@PropertySource("classpath:batch.properties")
+public class BatchConfiguration {
+    private final Environment env;
+
+    @Autowired
+    public BatchConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(org.postgresql.Driver.class.getName());
+        dataSource.setUrl(env.getProperty("dataSource.url"));
+        dataSource.setUsername(env.getProperty("dataSource.username"));
+        dataSource.setPassword(env.getProperty("dataSource.password"));
+        return dataSource;
+    }
+
+    private DatabasePopulator databasePopulator() {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.setContinueOnError(true);
+        populator.setIgnoreFailedDrops(true);
+        populator.addScript(new ClassPathResource("org/springframework/batch/core/schema-postgresql.sql"));
+        populator.addScript(new ClassPathResource("scripts/reset_user_registration.sql"));
+        return populator;
+    }
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+}
