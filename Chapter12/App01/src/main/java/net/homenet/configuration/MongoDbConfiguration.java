d44c930fae48e1b2e9d254065dee04c30c4e8e16
Lee Sangdae <mousesd@gmail.com>
2019-01-08 10:38:55 +0900

    Recipe12-1: #3) Use a MongoTemplate to simplify MongoDB code

--- Chapter12/App01/src/main/java/net/homenet/configuration/MongoDbConfiguration.java
+++ Chapter12/App01/src/main/java/net/homenet/configuration/MongoDbConfiguration.java
@@ -1,10 +1,12 @@
 package net.homenet.configuration;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.jndi.MongoClientFactory;
 import net.homenet.repository.MongoDBVehicleRepository;
 import net.homenet.repository.VehicleRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class MongoDbConfiguration {
@@ -16,7 +18,18 @@ public class MongoDbConfiguration {
     }
 
     @Bean
-    public VehicleRepository vehicleRepository(MongoClient mongo) {
-        return new MongoDBVehicleRepository(mongo, DB_NAME, "vehicles");
+    public MongoClientFactory mongoClientFactory() {
+        return new MongoClientFactory();
     }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, DB_NAME);
+    }
+
+    @Bean
+    public VehicleRepository vehicleRepository(MongoTemplate mongoTemplate) {
+        return new MongoDBVehicleRepository(mongoTemplate, "vehicles");
+    }
+
 }
--- Chapter12/App01/src/main/java/net/homenet/repository/MongoDBVehicleRepository.java
+++ Chapter12/App01/src/main/java/net/homenet/repository/MongoDBVehicleRepository.java
@@ -1,78 +1,52 @@
 package net.homenet.repository;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Filters;
-import com.sun.istack.internal.NotNull;
 import net.homenet.domain.Vehicle;
-import org.bson.Document;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 public class MongoDBVehicleRepository implements VehicleRepository {
-    private final MongoClient mongo;
+    private final MongoTemplate mongo;
     private final String collectionName;
-    private final String databaseName;
 
-    public MongoDBVehicleRepository(MongoClient mongo, String collectionName, String databaseName) {
+    public MongoDBVehicleRepository(MongoTemplate mongo, String collectionName) {
         this.mongo = mongo;
         this.collectionName = collectionName;
-        this.databaseName = databaseName;
     }
 
     @Override
     public long count() {
-        return getCollection().countDocuments();
+        return mongo.count(new Query(), collectionName);
     }
 
     @Override
     public void save(Vehicle vehicle) {
-        Document document = transform(vehicle);
-        getCollection().insertOne(document);
+        mongo.save(vehicle, collectionName);
     }
 
     @Override
     public void delete(Vehicle vehicle) {
-        getCollection().deleteOne(Filters.eq("vehicleNo", vehicle.getVehicleNo()));
+        mongo.remove(vehicle, collectionName);
     }
 
     @Override
     public List<Vehicle> findAll() {
-        return StreamSupport.stream(getCollection().find().spliterator(), false)
-            .map(this::transform)
-            .collect(Collectors.toList());
+        return mongo.findAll(Vehicle.class, collectionName);
     }
 
     @Override
     public Vehicle findByVehicleNo(String vehicleNo) {
-        Document document = getCollection().find(Filters.eq("vehicleNo", vehicleNo)).first();
-        assert document != null;
-        return transform(document);
-    }
-
-    private MongoCollection<Document> getCollection() {
-        return mongo.getDatabase(databaseName).getCollection(collectionName);
-    }
-
-    private Document transform(@NotNull Vehicle vehicle) {
-        return new Document("vehicleNo", vehicle.getVehicleNo())
-            .append("color", vehicle.getColor())
-            .append("wheel", vehicle.getWheel())
-            .append("seat", vehicle.getSeat());
-    }
-
-    private Vehicle transform(@NotNull Document document) {
-        return new Vehicle(document.getString("vehicleNo")
-            , document.getString("color")
-            , document.getInteger("wheel")
-            , document.getInteger("seat"));
+        return mongo.findOne(new Query(Criteria.where("vehicleNo").is(vehicleNo)), Vehicle.class, collectionName);
     }
 
     @PreDestroy
     public void cleanup() {
-        mongo.dropDatabase(databaseName);
+        mongo.execute(db -> {
+            db.drop();
+            return null;
+        });
     }
 }
