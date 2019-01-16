5495e30e8c97d30e8d991f2fb848cf417007e9a5
Lee Sangdae <mousesd@gmail.com>
2019-01-16 15:52:41 +0900

    App01: Recipe13-1: #4) Register MBean for remote access with RMI

--- Chapter13/App01/src/main/java/net/homenet/configuration/FileReplicatorConfiguration.java
+++ Chapter13/App01/src/main/java/net/homenet/configuration/FileReplicatorConfiguration.java
@@ -4,14 +4,20 @@ import net.homenet.FileCopier;
 import net.homenet.FileCopierImpl;
 import net.homenet.FileReplicator;
 import net.homenet.FileReplicatorImpl;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.jmx.support.ConnectorServerFactoryBean;
 import org.springframework.jmx.support.MBeanServerFactoryBean;
+import org.springframework.remoting.rmi.RmiRegistryFactoryBean;
 
 import javax.annotation.PostConstruct;
+import javax.management.remote.JMXConnectorServer;
 import java.io.File;
+import java.rmi.registry.Registry;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -53,6 +59,19 @@ public class FileReplicatorConfiguration {
         exporter.setBeans(beanToExport);
         exporter.setServer(mBeanServer.getObject());
         return exporter;
+    }
+
+    @Bean
+    public FactoryBean<Registry> rmiRegistry() {
+        return new RmiRegistryFactoryBean();
+    }
+
+    @Bean
+    @DependsOn("rmiRegistry")
+    public FactoryBean<JMXConnectorServer> connectorServer() {
+        ConnectorServerFactoryBean connectorServer = new ConnectorServerFactoryBean();
+        connectorServer.setServiceUrl("service:jmx:rmi://localhost/jndi/rmi://localhost:1099/replicator");
+        return connectorServer;
     }
 
     @SuppressWarnings("ResultOfMethodCallIgnored")
