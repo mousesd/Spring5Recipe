bd7670e732698553d9bba8478bdb291eebde3b6d
Lee Sangdae <mousesd@gmail.com>
2019-01-16 21:08:16 +0900

    App04: Recipe13-3: #2) Access remote MBeans through an MBean proxy

--- Chapter13/App04/src/main/java/net/homenet/Client.java
+++ Chapter13/App04/src/main/java/net/homenet/Client.java
@@ -4,21 +4,23 @@ import net.homenet.configuration.JmxClientConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import javax.management.Attribute;
-import javax.management.MBeanServerConnection;
-import javax.management.ObjectName;
-
 public class Client {
     public static void main(String[] args) throws Exception {
         ApplicationContext context = new AnnotationConfigApplicationContext(JmxClientConfiguration.class);
 
-        ObjectName mBeanName = new ObjectName("net.homenet:name=fileReplicator");
+        //# 1.Access remote MBeans through an MBean server connection
+        //ObjectName mBeanName = new ObjectName("net.homenet:name=fileReplicator");
+        //MBeanServerConnection mBeanServerConnection = context.getBean(MBeanServerConnection.class);
+        //mBeanServerConnection.addNotificationListener(mBeanName, new ReplicationNotificationListener(), null, null);
+        //
+        //String srcDir = (String) mBeanServerConnection.getAttribute(mBeanName, "SrcDir");
+        //mBeanServerConnection.setAttribute(mBeanName, new Attribute("DestDir", srcDir + "_backup"));
+        //mBeanServerConnection.invoke(mBeanName, "replicate", new Object[]{}, new String[]{});
 
-        MBeanServerConnection mBeanServerConnection = context.getBean(MBeanServerConnection.class);
-        mBeanServerConnection.addNotificationListener(mBeanName, new ReplicationNotificationListener(), null, null);
-
-        String srcDir = (String) mBeanServerConnection.getAttribute(mBeanName, "SrcDir");
-        mBeanServerConnection.setAttribute(mBeanName, new Attribute("DestDir", srcDir + "_backup"));
-        mBeanServerConnection.invoke(mBeanName, "replicate", new Object[]{}, new String[]{});
+        //# 2.Access remote MBeans through an MBean proxy
+        FileReplicator fileReplicator = context.getBean(FileReplicator.class);
+        String srcDir = fileReplicator.getSrcDir();
+        fileReplicator.setDestDir(srcDir + "_backup");
+        fileReplicator.replicate();
     }
 }
--- Chapter13/App04/src/main/java/net/homenet/configuration/JmxClientConfiguration.java
+++ Chapter13/App04/src/main/java/net/homenet/configuration/JmxClientConfiguration.java
@@ -1,11 +1,14 @@
 package net.homenet.configuration;
 
+import net.homenet.FileReplicator;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.access.MBeanProxyFactoryBean;
 import org.springframework.jmx.support.MBeanServerConnectionFactoryBean;
 
 import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
 import java.net.MalformedURLException;
 
 @Configuration
@@ -15,5 +18,14 @@ public class JmxClientConfiguration {
         MBeanServerConnectionFactoryBean factoryBean = new MBeanServerConnectionFactoryBean();
         factoryBean.setServiceUrl("service:jmx:rmi://localhost/jndi/rmi://localhost:1099/replicator");
         return factoryBean;
+    }
+
+    @Bean
+    public MBeanProxyFactoryBean fileReplicatorProxy(MBeanServerConnection mBeanServerConnection) throws MalformedObjectNameException {
+        MBeanProxyFactoryBean fileReplicatorProxy = new MBeanProxyFactoryBean();
+        fileReplicatorProxy.setServer(mBeanServerConnection);
+        fileReplicatorProxy.setObjectName("net.homenet:name=fileReplicator");
+        fileReplicatorProxy.setProxyInterface(FileReplicator.class);
+        return fileReplicatorProxy;
     }
 }
