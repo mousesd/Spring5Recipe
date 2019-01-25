d187fde41aa2cfbb470817c6172ac42295333915
Lee Sangdae <mousesd@gmail.com>
2019-01-25 14:21:25 +0900

    Recipe14-7(App26): #2) Listen to messages using Spring Kafka

--- Chapter14/App26/src/main/java/net/homenet/MailListener.java
+++ Chapter14/App26/src/main/java/net/homenet/MailListener.java
@@ -4,7 +4,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 public class MailListener {
     @KafkaListener(topics = "mails")
-    public void onMessage(Mail mail) {
+    public void onMessage(String mail) {
         System.out.println("Received: " + mail);
     }
 }
