282e73d044163d0b88765f4f8587219cc21e0235
Lee Sangdae <mousesd@gmail.com>
2019-02-12 09:03:09 +0900

    Recipe16-7(App13): #1) Access the database in integration tests(JUnit4)

---
+++ Chapter16/App13/src/main/java/net/homenet/repository/InMemoryAccountDaoImpl.java
@@ -0,0 +1,53 @@
+package net.homenet.repository;
+
+import net.homenet.domain.Account;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings({ "unused", "WeakerAccess" })
+public class InMemoryAccountDaoImpl implements AccountDao {
+    private Map<String, Account> accounts;
+
+    public InMemoryAccountDaoImpl() {
+        accounts = Collections.synchronizedMap(new HashMap<>());
+    }
+
+    @Override
+    public void createAccount(Account account) {
+        if (accountExists(account.getAccountNo())) {
+            throw new DuplicateAccountException();
+        }
+        accounts.put(account.getAccountNo(), account);
+    }
+
+    @Override
+    public void updateAccount(Account account) {
+        if (!accountExists(account.getAccountNo())) {
+            throw new AccountNotFoundException();
+        }
+        accounts.put(account.getAccountNo(), account);
+    }
+
+    @Override
+    public void removeAccount(Account account) {
+        if (!accountExists(account.getAccountNo())) {
+            throw new AccountNotFoundException();
+        }
+        accounts.remove(account.getAccountNo());
+    }
+
+    @Override
+    public Account findAccount(String accountNo) {
+        Account account = accounts.get(accountNo);
+        if (account == null) {
+            throw new AccountNotFoundException();
+        }
+        return account;
+    }
+
+    public boolean accountExists(String accountNo) {
+        return accounts.containsKey(accountNo);
+    }
+}
---
+++ Chapter16/App13/src/test/java/net/homenet/service/AccountServiceJUnit4SpringContextTests.java
@@ -0,0 +1,50 @@
+package net.homenet.service;
+
+import net.homenet.configuration.BankConfiguration;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import static org.junit.Assert.*;
+
+@SuppressWarnings("SqlDialectInspection")
+@ContextConfiguration(classes = BankConfiguration.class)
+@Sql(scripts = { "classpath:/ddl.sql" })
+public class AccountServiceJUnit4SpringContextTests extends AbstractTransactionalJUnit4SpringContextTests {
+
+    private static final String TEST_ACCOUNT_NO = "1234";
+    private AccountService accountService;
+
+    @Autowired
+    public void setAccountService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Before
+    public void setUp() {
+        jdbcTemplate.update("INSERT INTO ACCOUNT (ACCOUNT_NO, BALANCE) VALUES (?, ?)", TEST_ACCOUNT_NO, 100);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void deposit() {
+        accountService.deposit(TEST_ACCOUNT_NO, 50);
+        Double balance = jdbcTemplate.queryForObject("SELECT BALANCE FROM ACCOUNT WHERE ACCOUNT_NO = ?"
+            , Double.class
+            , TEST_ACCOUNT_NO);
+        assertEquals(balance, 150.0, 0);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void withdraw() {
+        accountService.withdraw(TEST_ACCOUNT_NO, 50);
+        Double balance = jdbcTemplate.queryForObject("SELECT BALANCE FROM ACCOUNT WHERE ACCOUNT_NO = ?"
+            , Double.class
+            , TEST_ACCOUNT_NO);
+        assertEquals(balance, 50.0, 0);
+    }
+}
